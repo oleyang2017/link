c874df6f56a498fa310f18746595901d48624709
fanmlei <15638515832@163.com>
2020-11-16 23:19:56 +0800

    引入debug_toolbar

--- LinkIoT/LinkIoT/settings.py
+++ LinkIoT/LinkIoT/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '*ahhox%k)*#to$q2ms)kuqkg^74b0bm90=%1#8d^fbt84&p85q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'LinkIoT.urls'
