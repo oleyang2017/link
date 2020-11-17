6b50145d1b0cbfa40efb1e86b5f3db5ae7f312c9
fanmlei <15638515832@163.com>
2020-11-18 01:29:55 +0800

    [ADD] 自定义User模型，增加微信小程序配置项

--- LinkIoT/LinkIoT/settings.py
+++ LinkIoT/LinkIoT/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -29,7 +28,6 @@ ALLOWED_HOSTS = [
     '127.0.0.1'
 ]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar'
+    'debug_toolbar',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -73,17 +72,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'LinkIoT.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'your database name',
+        'USER': 'your username',
+        'PASSWORD': 'your password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'OPTIONS': {
+            'client_encoding': 'UTF8'
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -103,6 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# 设置自定义的User模型
+AUTH_USER_MODEL = 'user.UserProfile'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
@@ -117,8 +123,16 @@ USE_L10N = True
 
 USE_TZ = False
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 微信小程序appid和secret
+WX_APPID = 'your wechat miniprogram appid'
+WX_SECRET = 'your wechat miniprogram secret'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
---
+++ LinkIoT/user/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserProfile(AbstractUser):
+    """
+    用户信息
+    """
+    GENDER_CHOICES = (
+        ("0", "未知"),
+        ("1", "男"),
+        ("2", "女")
+    )
+    mobile = models.CharField(verbose_name="手机号", max_length=20, unique=True, blank=True, null=True)
+    wx_open_id = models.CharField(verbose_name="wx_openid", max_length=64, default='', blank=True, db_index=True)
+    gender = models.CharField(verbose_name='性别', max_length=6, choices=GENDER_CHOICES, default='1')
+    desc = models.TextField(verbose_name='个人介绍', blank=True, default='这个人很懒，什么都没写！')
+    avatar = models.ImageField(verbose_name='头像', upload_to='avatar/', blank=True, null=True)
+    avatar_url = models.URLField(verbose_name=' 微信头像', blank=True, null=True)
+    address = models.CharField(verbose_name='地址', max_length=128, default='', blank=True)
+    create_time = models.DateTimeField(verbose_name='创建日期', auto_now_add=True)
+
+    class Meta(AbstractUser.Meta):
+        swappable = 'AUTH_USER_MODEL'
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
