1a06b4a9e109cf8187da881b1fd4c14b32cfc62c
fanmlei <15638515832@163.com>
2020-11-29 23:07:14 +0800

    增加简单的数据集模型，可直接使用，或者日后更改为时序数据库

---
+++ LinkIoT/device/models/data.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class DataSet(models.Model):
+
+    stream_id = models.CharField(max_length=64, verbose_name='数据流')  # 这里不和stream模型做建立关联,目的是便于日后使用时序数据库
+    client_id = models.CharField(max_length=32, verbose_name='设备')  # 非device_id
+    topic = models.CharField(max_length=100, verbose_name='Topic')
+    time = models.DateTimeField(auto_now_add=True, verbose_name='数据上传时间')
+
+    float_data = models.FloatField(null=True, blank=True)
+    json_data = models.JSONField(null=True, blank=True)
+    int_data = models.IntegerField(null=True, blank=True)
+    bool_data = models.BooleanField(null=True, blank=True)
+    char_data = models.CharField(max_length=64, null=True, blank=True)
