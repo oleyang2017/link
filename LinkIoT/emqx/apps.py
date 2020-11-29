b57f001fe7cc6cba47e0314a959343f175422162
fanmlei <15638515832@163.com>
2020-11-29 23:05:51 +0800

    增加EMQX，使用EMQX作为MQTT消息中间件时可以开启，内置ACL表

---
+++ LinkIoT/emqx/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EmqxConfig(AppConfig):
+    name = 'emqx'
---
+++ LinkIoT/emqx/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from device.models import Stream, Device
+
+
+class MQTTAcl(models.Model):
+    # EMQ权限控制表
+    ACCESS_CHOICE = (
+        (1, '可订阅'),
+        (2, '可发布'),
+        (3, '两者皆可'),
+    )
+    allow = models.IntegerField(blank=True, default=1)  # 1:允许, 0:禁止
+    ipaddr = models.CharField(max_length=60, blank=True, default='')  # ip地址
+    username = models.CharField(max_length=100, blank=True, default='')  # 用户名
+    clientid = models.CharField(max_length=100, blank=True, default='')  # 设备名
+    access = models.IntegerField(choices=ACCESS_CHOICE, default=2)  # 1：可订阅  2：可发布  3 两者皆可
+    topic = models.CharField(max_length=100)  # 主题名
+
+    class Meta:
+        db_table = 'mqtt_acl'
+
+
+@receiver(post_save, sender=Device, dispatch_uid='add_acl_device')
+def add_acl(sender, instance, **kwargs):
+    # 设备注册的数据流topic
+    MQTTAcl.objects.create(allow=1, clientid=instance.device.client_id,
+                           access=2, topic='{client_id}/#'.format(client_id=instance.client_id))
+
+
+@receiver(post_delete, sender=Device, dispatch_uid='remove_acl_device')
+def remove_acl(sender, instance, **kwargs):
+    # 设备删除的数据流topic
+    MQTTAcl.objects.filter(topic='{client_id}/#'.format(client_id=instance.client_id)).delete()
