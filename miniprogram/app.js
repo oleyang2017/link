6132cf6cc076240c38448b2c8cbac01f47967fe9
fanmlei <15638515832@163.com>
2020-12-17 23:46:24 +0800

    微信小程序的登录流程

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,39 +1,30 @@
-//app.js
+import auth from './api/auth'
+
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+    // 检查token是否过期
+    let token = wx.getStorageSync('token') || ""
+    let refresh = wx.getStorageSync('refresh') || ""
+    if (token){
+      auth.verify({token}).then((res)=>{
+        if ('detail' in res){
+          // 尝试刷新token
+          if (refresh){
+            auth.refresh({refresh}).then((res)=>{
+              if ('detail' in res){
+                // 跳转登录页面
+                console.log('to login page')
               }
-            }
-          })
+              if ('access' in res){
+                wx.setStorageSync('token', res.access)
+              }
+            })
+          }
         }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
+      })
+    } else {
+      console.log('no token to login page!')
+    }
   }
+    
 })
