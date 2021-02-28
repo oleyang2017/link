1e672f5952b5b164e0ffe1613f55cf288a68b946
fanmlei <15638515832@163.com>
2021-03-01 00:09:20 +0800

    未分类的设备显示

--- miniprogram/pages/device/index.js
+++ miniprogram/pages/device/index.js
@@ -21,19 +21,26 @@ Page({
   async getCategory() {
     let category = await categoryApi.list()
     let device = await deviceApi.list()
+    let noCategory = []
     for (let i = 0; i < device.length; i++) {
+      let hasCategory = false
       for (let m = 0; m < category.length; m++) {
         if (!category[m].device) {
           category[m].device = []
         }
         if (device[i].category == category[m].id) {
           category[m].device = [device[i]]
+          hasCategory = true
         }
       }
+      if (!hasCategory){
+        noCategory.push(device[i])
+      }
     }
+    category.push({name: '未分类', device: noCategory, device_count: noCategory.length})
     this.setData({
-      category: category.length ? category : [{name: '全部设备'}],
-      popupHeight: category.length * 36 > 260 ? '260px' : category.length * 36 + 'px'
+      category,
+      popupHeight: (category.length - 1) * 36 > 260 ? '260px' : (category.length - 1) * 36 + 'px'
     })
   },
   changeCategory(e) {
@@ -41,4 +48,9 @@ Page({
       active: e.detail.name ? e.detail.name : e.currentTarget.dataset.id
     })
   },
+  toDetail(e) {
+    wx.navigateTo({
+      url: '/pages/device/detail/index?id=' + e.currentTarget.dataset.id,
+    })
+  }
 })
