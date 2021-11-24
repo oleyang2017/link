ceeff1d8b1972815ff9450945e2ec43e1ad031ad
fanmlei <15638515832@163.com>
2021-11-24 23:44:55 +0800

    更新shortuuid版本，移除自建扩展字段，使用自带的

--- link/apps/device/models/device.py
+++ link/apps/device/models/device.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.conf import settings
-from base.base_model import BaseManager
-from utils.field_extend import ShortUUIDField
+from base.base_model import BaseModel
+from shortuuid.django_fields import ShortUUIDField
 
 
-class Device(models.Model):
+class Device(BaseModel):
     """ 设备 """
-    create_user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='devices', verbose_name='创建人', on_delete=models.CASCADE)
-    client_id = models.CharField(max_length=32, verbose_name="client_id")
+    client_id = ShortUUIDField(max_length=12, verbose_name="client_id")
+    device_id = ShortUUIDField(max_length=12, verbose_name="device_id")
     category = models.ForeignKey("device.DeviceCategory", null=True, blank=True, related_name='devices', verbose_name='所属分类', on_delete=models.SET_NULL)
     name = models.CharField(max_length=8, verbose_name='设备名称')
     desc = models.TextField(blank=True, default='', verbose_name='说明')
@@ -18,7 +18,7 @@ class Device(models.Model):
     create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
     update_time = models.DateTimeField(auto_now=True, verbose_name='更新时间')
     last_connect_time = models.DateTimeField(verbose_name='最新连接时间', null=True, blank=True)
-    token = models.CharField(max_length=32, verbose_name="token")
+    token = ShortUUIDField(max_length=16, verbose_name="token")
 
     class Meta:
         verbose_name = '设备'
