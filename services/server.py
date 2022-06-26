efe6ac7322cba57d50d9813f77ac3a4f028c8aa0
fanmlei <15638515832@163.com>
2022-06-27 02:56:09 +0800

    style: format python code

--- services/server.py
+++ services/server.py
@@ -1,14 +1,12 @@
-from concurrent import futures
 import logging
+from concurrent import futures
 
 import grpc
-
 import exhook_pb2
 import exhook_pb2_grpc
 
 
 class HookProvider(exhook_pb2_grpc.HookProviderServicer):
-
     def OnClientConnect(self, request, context):
         print(">>>>OnClientConnect")
         print(request)
@@ -24,7 +22,6 @@ class HookProvider(exhook_pb2_grpc.HookProviderServicer):
         print(request)
         return exhook_pb2.EmptySuccess()
 
-
     def OnClientSubscribe(self, request, context):
         return exhook_pb2.ClientSubscribeRequest()
 
@@ -32,12 +29,13 @@ class HookProvider(exhook_pb2_grpc.HookProviderServicer):
         print("sss")
         return exhook_pb2.LoadedResponse(
             hooks=[
-                exhook_pb2.HookSpec(name=b'client.connect', topics=[]),
-                exhook_pb2.HookSpec(name=b'client.connected', topics=[]),
-                exhook_pb2.HookSpec(name=b'client.disconnected', topics=[]),
-                exhook_pb2.HookSpec(name=b'message.publish', topics=[]),
-                exhook_pb2.HookSpec(name=b'message.acked', topics=[]),
-            ])
+                exhook_pb2.HookSpec(name=b"client.connect", topics=[]),
+                exhook_pb2.HookSpec(name=b"client.connected", topics=[]),
+                exhook_pb2.HookSpec(name=b"client.disconnected", topics=[]),
+                exhook_pb2.HookSpec(name=b"message.publish", topics=[]),
+                exhook_pb2.HookSpec(name=b"message.acked", topics=[]),
+            ]
+        )
 
     def OnMessagePublish(self, request, context):
         print("publish")
@@ -63,12 +61,12 @@ class HookProvider(exhook_pb2_grpc.HookProviderServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
     exhook_pb2_grpc.add_HookProviderServicer_to_server(HookProvider(), server)
-    server.add_insecure_port('[::]:9000')
+    server.add_insecure_port("[::]:9000")
     server.start()
     server.wait_for_termination()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig()
     print(222)
     serve()
