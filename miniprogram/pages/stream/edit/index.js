63af43dd1a42c7baca7d9b676369d9ee79956f50
fanmlei <15638515832@163.com>
2022-07-15 01:12:39 +0800

    feat: 数据流可以自定义背景、图标等信息

--- miniprogram/pages/stream/edit/index.js
+++ miniprogram/pages/stream/edit/index.js
@@ -39,7 +39,9 @@ Page({
     ],
     selectList: [],
     selectName: '',
-    show: false,
+    showPopup: false,
+    showIconPopup: false,
+    showColorPopup: false,
     title: '',
     qos: 0,
     data_type: 'int',
@@ -49,6 +51,170 @@ Page({
     defaultQosIndex: 0,
     defaultQos: '0',
     defaultType: '整型（int）',
+    color: "bg-blue",
+    icon: "icon-kongqiwendu",
+    iconList: [
+      {
+        "name": "电导率",
+        "value": "icon-diandaoshuai"
+      },
+      {
+        "name": "PH值",
+        "value": "icon-PHzhi"
+      },
+      {
+        "name": "含氧量",
+        "value": "icon-hanyangliang"
+      }, {
+        "name": "水位",
+        "value": "icon-shuiwei"
+      },
+      {
+        "name": "库容",
+        "value": "icon-kurong"
+      },
+      {
+        "name": "浊度",
+        "value": "icon-zhuodu"
+      },
+      {
+        "name": "溶解氧",
+        "value": "icon-rongjieyang"
+      },
+      {
+        "name": "压力",
+        "value": "icon-yali"
+      },
+      {
+        "name": "悬浮物",
+        "value": "icon-xuanfuwu"
+      },
+      {
+        "name": "流量",
+        "value": "icon-liuliang"
+      },
+      {
+        "name": "水温",
+        "value": "icon-shuiwen"
+      },
+      {
+        "name": "土壤水分",
+        "value": "icon-turangshuifen"
+      },
+      {
+        "name": "土壤湿度",
+        "value": "icon-turangshidu"
+      },
+      {
+        "name": "风速",
+        "value": "icon-fengsu"
+      },
+      {
+        "name": "电源电压",
+        "value": "icon-dianyuandianya"
+      },
+      {
+        "name": "CO2浓度",
+        "value": "icon-CO2nongdu"
+      },
+      {
+        "name": "大气压",
+        "value": "icon-daqiya"
+      },
+      {
+        "name": "光合辐射",
+        "value": "icon-guanghefushe"
+      },
+      {
+        "name": "结露",
+        "value": "icon-jielou"
+      },
+      {
+        "name": "空气温度",
+        "value": "icon-kongqiwendu"
+      },
+      {
+        "name": "空气湿度",
+        "value": "icon-kongqishidu"
+      },
+      {
+        "name": "风向",
+        "value": "icon-fengxiang"
+      },
+      {
+        "name": "降雨量",
+        "value": "icon-jiangyuliang"
+      },
+      {
+        "name": "露点温度",
+        "value": "icon-loudianwendu"
+      },
+      {
+        "name": "光照强度",
+        "value": "icon-guangzhaoqiangdu"
+      },
+      {
+        "name": "蒸发量",
+        "value": "icon-zhengfaliang"
+      },
+      {
+        "name": "总辐射",
+        "value": "icon-zongfushe"
+      }, {
+        "name": "光照时间",
+        "value": "icon-guangzhaoshijian"
+      },
+      {
+        "name": "PM2.5",
+        "value": "icon-PM25"
+      },
+      {
+        "name": "小时降雨量",
+        "value": "icon-xiaoshijiangyuliang"
+      },
+      {
+        "name": "风力",
+        "value": "icon-fengli"
+      },
+      {
+        "name": "硫化氢",
+        "value": "icon-liuhuaqing"
+      },
+    ],
+    colorList: [
+      "bg-red",
+      "bg-orange",
+      "bg-yellow",
+      "bg-olive",
+      "bg-green",
+      "bg-cyan",
+      "bg-blue",
+      "bg-purple",
+      "bg-mauve",
+      "bg-pink",
+      "bg-brown",
+      "bg-grey",
+      "bg-black",
+      "bg-gray",
+      "bg-red light",
+      "bg-orange light",
+      "bg-yellow light",
+      "bg-olive light",
+      "bg-green light",
+      "bg-cyan light",
+      "bg-blue light",
+      "bg-purple light",
+      "bg-mauve light",
+      "bg-pink light",
+      "bg-brown light",
+      "bg-grey light",
+      "bg-gradual-red",
+      "bg-gradual-orange",
+      "bg-gradual-green",
+      "bg-gradual-purple",
+      "bg-gradual-pink",
+      "bg-gradual-blue",
+    ]
   },
 
   onLoad: function (options) {
@@ -99,6 +265,11 @@ Page({
       })
     })
   },
+  changeIconPopup(e){
+    this.setData({
+      showIconPopup: !this.data.showIconPopup
+    })
+  },
   openPopup(e) {
     if (this.data.type == 'edit') {
       return
@@ -126,7 +297,7 @@ Page({
       })
     }
     this.setData({
-      show: !this.data.show
+      showPopup: !this.data.showPopup
     })
 
   },
@@ -163,7 +334,10 @@ Page({
         unit_name: this.data.unit_name,
         unit: this.data.unit,
         qos: this.data.qos,
-        data_type: this.data.data_type
+        data_type: this.data.data_type,
+        icon: this.data.icon,
+        color: this.data.color,
+        show: this.data.show,
       }).then(() => {
         Toast({
           type: 'success',
@@ -180,7 +354,10 @@ Page({
         device: this.data.device,
         unit: this.data.unit,
         qos: this.data.qos,
-        data_type: this.data.data_type
+        data_type: this.data.data_type,
+        icon: this.data.icon,
+        color: this.data.color,
+        show: this.data.show,
       }).then(() => {
         Toast({
           type: 'success',
@@ -200,9 +377,12 @@ Page({
         unit: this.data.unit,
         unit_name: this.data.unit_name,
         qos: this.data.qos,
-        data_type: this.data.data_type
+        data_type: this.data.data_type,
+        icon: this.data.icon,
+        color: this.data.color,
+        show: this.data.show,
       }
-      if (this.data.id){
+      if (this.data.id) {
         stream.id = this.data.id
       }
       streamList.push(stream)
@@ -211,6 +391,30 @@ Page({
       })
       wx.navigateBack()
     }
+  },
+  changeShow({
+    detail
+  }) {
+    this.setData({
+      show: detail
+    });
+  },
+  selectIcon(e){
+    this.setData({
+      icon: e.currentTarget.dataset.value,
+      showIconPopup: false
+    })
+  },
+  changeColorPopup(e){
+    this.setData({
+      showColorPopup: !this.data.showColorPopup
+    })
+  },
+  selectColor(e){
+    this.setData({
+      color: e.currentTarget.dataset.value,
+      showColorPopup: false
+    })
   },
   cancel() {
     wx.navigateBack()
