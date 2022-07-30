58a9ff7be6687d182fd8b832f8c6e16a63f56fda
fanmlei <15638515832@163.com>
2022-07-31 03:30:32 +0800

    feat: 邀请链接列表,详情页展示

---
+++ miniprogram/api/invite.js
@@ -0,0 +1,22 @@
+import { request } from './request';
+
+export default {
+  list: (params) => {
+    return request('/api/invite_links/', params, 'GET')
+  },
+  detail:(id) => { 
+    return request(`/api/invite_links/${id}/`, {}, "GET") 
+  },
+  recordList: (id) => {
+    return request(`/api/invite_links/${id}/record_list/`, params, 'GET')
+  },
+  create:(params) => { 
+    return request('/api/invite_links/', params, "POST") 
+  },
+  update: (data) => {
+    return request(`/api/invite_links/${data.id}/`, data, "PUT")
+  },
+  inviteUser: (data) => {
+    return request(`/api/invite_links/${data.id}/invite_user/`, data, "POST")
+  },
+}
