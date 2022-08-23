430459990478193d23640448bc4c96ffb4c25288
FanMinglei <15638515832@163.com>
2022-08-23 09:32:37 +0000

    test: 增加stream单元测试

---
+++ link/apps/device/tests/test_stream.py
@@ -0,0 +1,78 @@
+from django.test import TestCase
+from django.db.utils import IntegrityError
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from device.models.stream import Stream
+from device.models.device import Device
+from user.models.user_profile import UserProfile
+
+
+class StreamModelTestCase(TestCase):
+    def setUp(self):
+        self.user = UserProfile.objects.create(username="test_stream_user")
+        self.device = Device.objects.create(name="d", create_user=self.user)
+
+    def test_create_stream(self):
+        Stream.objects.create(device=self.device, create_user=self.user, name="s")
+        try:
+            Stream.objects.create(device=self.device, create_user=self.user, name="s")
+        except IntegrityError:
+            pass
+
+
+class StreamAPITestCase(APITestCase):
+    def setUp(self):
+        self.user = UserProfile.objects.create(username="test_stream_api_user")
+        self.user2 = UserProfile.objects.create(username="test_stream_api_user_2")
+        self.client.force_authenticate(user=self.user)
+
+    def test_stream_list(self):
+        response = self.client.post("/api/devices/", {"name": 1}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        device_id = response.data.get("id")
+        device = Device.objects.filter(id=device_id).first()
+        Stream.objects.create(device=device, create_user=self.user, name="s")
+        Stream.objects.create(device=device, create_user=self.user, name="s1")
+        Stream.objects.create(device=device, create_user=self.user2, name="s3")
+        response = self.client.get("/api/streams/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+        device_id = device.id
+        response = self.client.get(f"/api/devices/{device_id}/streams/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+
+    def test_stream_create(self):
+        response = self.client.post("/api/devices/", {"name": 1}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        device_id = response.data.get("id")
+        response = self.client.post("/api/streams/", {"name": "1", "device": device_id})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.client.post("/api/streams/", {"name": "1", "device": device_id})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["non_field_errors"][0],  "同一设备数据流名称不能重复！")
+
+        response = self.client.post("/api/streams/", {"name": "2"})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["device"][0], "该字段是必填项。")
+    
+    def test_stream_update(self):
+        response = self.client.post("/api/devices/", {"name": 1}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        device = Device.objects.filter(id=response.data.get("id")).first()
+        stream1 = Stream.objects.create(name="s1", device=device, create_user=self.user)
+        stream2 = Stream.objects.create(name="s2", device=device, create_user=self.user2)
+
+        response = self.client.put(f"/api/streams/{stream1.id}/", {"name": "s2"})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["non_field_errors"][0],  "同一设备数据流名称不能重复！")
+
+        response = self.client.put(f"/api/streams/{stream1.id}/", {"name": "s3"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.put(f"/api/streams/{stream1.id}/", {"device": 1})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        response = self.client.put(f"/api/streams/{stream1.id}/", {"data_type": "char"})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
