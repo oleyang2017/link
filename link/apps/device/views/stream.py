58aa7e19dc768188c27b259df20745daff07f258
fanmlei <15638515832@163.com>
2022-08-24 00:20:39 +0800

    fix: 权限相关

--- link/apps/device/views/stream.py
+++ link/apps/device/views/stream.py
@@ -1,11 +1,12 @@
-from rest_framework.filters import OrderingFilter
+from django.db.models import Q
 from guardian.shortcuts import assign_perm, get_user_perms, get_objects_for_user
+from rest_framework.filters import OrderingFilter
 from rest_framework.exceptions import PermissionDenied
 from django_filters.rest_framework import DjangoFilterBackend
 
 from base.base_viewsets import BaseModelViewSet
-from device.models.stream import Stream
 from device.models.device import Device
+from device.models.stream import Stream
 from device.serializers.stream import StreamSerializer
 
 
@@ -18,12 +19,7 @@ class StreamViewSet(BaseModelViewSet):
 
     def get_queryset(self):
         device_objets = get_objects_for_user(self.request.user, perms="view_device", klass=Device)
-        print(device_objets)
-        queryset = device_objets.streams
-        queryset += Stream.objects.filter(create_user=self.request.user)
-        # if self.request.query_params.get("only_creator", False):
-        #     q_set = q_set.filter(create_user=self.request.user)
-        return queryset
+        return Stream.objects.filter(Q(device__in=device_objets) | Q(create_user=self.request.user))
 
     def perform_update(self, serializer):
         if not self.request.user.has_perm("change_device", serializer.instance.device):
@@ -35,9 +31,9 @@ class StreamViewSet(BaseModelViewSet):
             raise PermissionDenied("你没有删除权限")
         return super(StreamViewSet, self).perform_destroy(instance)
 
-    # def perform_create(self, serializer):
-    #     print(serializer)
-    #     print(serializer.data)
-    #     if not self.request.user.has_perm("change_device", serializer.data["device"]):
-    #         raise PermissionDenied("你没有添加权限")
-    #     return super(StreamViewSet, self).perform_create(serializer)
+    def perform_create(self, serializer):
+        device_id = self.request.data.get("device")
+        device = Device.objects.filter(id=device_id).first()
+        if not self.request.user.has_perm("change_device", device):
+            raise PermissionDenied("你没有添加权限")
+        return super(StreamViewSet, self).perform_create(serializer)
