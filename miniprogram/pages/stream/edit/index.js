414217532f446e882bf7f2abc326d295e1a437bf
fanminglei <15638515832@163.com>
2023-01-03 17:47:28 +0800

    polish: 图表自适应屏幕宽度

--- miniprogram/pages/stream/edit/index.js
+++ miniprogram/pages/stream/edit/index.js
@@ -19,16 +19,21 @@ function initChart(canvas, width, height, dpr) {
   canvas.setChart(chart);
   var option = {
     title: {
-      text: '测试下面legend的红色区域不应被裁剪',
-      left: 'center'
+      text: "图表标题",
+      left: "center",
+      textStyle:{
+        fontSize :14
+      },
+      top: "bottom",
+      subtext: "图表名称"
     },
-    legend: {
-      data: ['A', 'B', 'C'],
-      top: 50,
-      left: 'center',
-      backgroundColor: 'red',
-      z: 100
-    },
+    // legend: {
+    //   data: ['A'],
+    //   top: 50,
+    //   left: 'center',
+    //   backgroundColor: 'red',
+    //   z: 100
+    // },
     grid: {
       containLabel: true
     },
@@ -37,10 +42,18 @@ function initChart(canvas, width, height, dpr) {
       trigger: 'axis'
     },
     xAxis: {
-      type: 'category',
+      type: 'time',
       boundaryGap: false,
-      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-      // show: false
+      data: [
+        '2023-01-01 00:00:00', 
+        '2023-01-01 01:00:00', 
+        '2023-01-01 02:00:00', 
+        '2023-01-01 03:00:00',
+        '2023-01-01 04:00:00',
+        '2023-01-01 05:00:00',
+        '2023-01-01 06:00:00',
+        '2023-01-01 07:00:00',
+      ],
     },
     yAxis: {
       x: 'center',
@@ -50,23 +63,12 @@ function initChart(canvas, width, height, dpr) {
           type: 'dashed'
         }
       }
-      // show: false
     },
     series: [{
       name: 'A',
       type: 'line',
       smooth: true,
       data: [18, 36, 65, 30, 78, 40, 33]
-    }, {
-      name: 'B',
-      type: 'line',
-      smooth: true,
-      data: [12, 50, 51, 35, 70, 30, 20]
-    }, {
-      name: 'C',
-      type: 'line',
-      smooth: true,
-      data: [10, 30, 31, 50, 40, 20, 10]
     }]
   };
 
@@ -91,19 +93,21 @@ Page({
     icon: "icon-kongqiwendu",
     show: false,
     saveData: false,
-    showChart: false,
+    showChart: true,
     themeInputStyle: {
       maxHeight: 200
     },
     ec: {
       onInit: initChart
-    }
+    },
+    windowWidth: 320,
   },
 
   onLoad: function (options) {
-    console.log(options)
+    var res = wx.getSystemInfoSync();
     this.setData({
-      ...options
+      ...options,
+      windowWidth: res.windowWidth - 30
     })
     if (options.id) {
       streamApi.detail(options.id).then((res) => {
@@ -127,6 +131,7 @@ Page({
         deviceList: res
       })
     })
+  
   },
 
   openPopup(e) {
@@ -147,7 +152,6 @@ Page({
         popupType: e.currentTarget.dataset.name,
       })
     }
-
   },
   changeSelect(e) {
     const {
