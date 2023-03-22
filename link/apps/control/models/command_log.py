ed1f10b1165c54969755cf87b3180863e63ac627
fanmlei <15638515832@163.com>
2023-03-22 23:52:41 +0800

    polish: 调整日志结构

--- link/apps/control/models/command_log.py
+++ link/apps/control/models/command_log.py
@@ -1,5 +1,6 @@
+import uuid
+
 from django.db import models
-from django.conf import settings
 
 
 class CommandLog(models.Model):
@@ -12,9 +13,21 @@ class CommandLog(models.Model):
         on_delete=models.SET_NULL,
         db_constraint=False,
     )
-    content = models.TextField(verbose_name="触发原因", blank=False, null=False)
+    automation_log = models.ForeignKey(
+        "control.AutomationLog",
+        null=True,
+        blank=True,
+        related_name="command_logs",
+        verbose_name="自动化日志",
+        on_delete=models.CASCADE,
+        db_constraint=False,
+        db_index=True,
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    payload = models.TextField(verbose_name="下发指令内容", blank=False, null=False)
     result = models.TextField(blank=True, default="", verbose_name="执行结果")
     created_time = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
+    callback_time = models.DateTimeField(verbose_name="回调时间", null=True, blank=True)
 
     class Meta:
         db_table = "command_log"
