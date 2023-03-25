24cdf64671aa55c9be08ccdb95bc5982a76ffc6e
fanmlei <15638515832@163.com>
2023-03-25 18:39:32 +0800

    test: 增加修改测试用例

--- link/apps/device/tests/test_device.py
+++ link/apps/device/tests/test_device.py
@@ -3,6 +3,7 @@ from rest_framework import status
 from guardian.shortcuts import assign_perm, get_objects_for_user
 from rest_framework.test import APITestCase
 
+from device.models.chart import Chart
 from device.models.device import Device
 from device.models.stream import Stream
 from device.models.category import DeviceCategory
@@ -14,9 +15,9 @@ class DeviceModelTestCase(TestCase):
         self.user = UserProfile.objects.create(username="test_device_user")
 
     def test_create_device(self):
-        categroy = DeviceCategory.objects.create(name="c", create_user=self.user)
+        category = DeviceCategory.objects.create(name="c", create_user=self.user)
         Device.objects.create(name="1", create_user=self.user)
-        Device.objects.create(name="1", create_user=self.user, category=categroy)
+        Device.objects.create(name="1", create_user=self.user, category=category)
 
 
 class DeviceAPITestCase(APITestCase):
@@ -62,6 +63,7 @@ class DeviceAPITestCase(APITestCase):
         for i in response.data:
             self.assertEqual(i.get("create_user"), self.user.id)
         self.assertEqual(len(response.data), count)
+
         # 权限过滤
         assign_perm("change_device", self.user, self.share_device)
         assign_perm("change_device", self.user, device)
@@ -72,11 +74,12 @@ class DeviceAPITestCase(APITestCase):
         self.assertEqual(len(response.data), count)
 
         perms = ["sub", "view_device"]
-        response = self.client.get("/api/devices/", {"perms": perms}, format="json")
+        response = self.client.get("/api/devices/", {"perms": perms})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         count = len(get_objects_for_user(self.user, perms=perms, klass=Device))
         self.assertEqual(len(response.data), count)
 
+        # 权限过滤 + 只获取自己创建的设备
         perms = ["change_device"]
         response = self.client.get(
             "/api/devices/",
@@ -89,52 +92,112 @@ class DeviceAPITestCase(APITestCase):
         self.assertEqual(len(response.data), count)
 
     def test_create_device(self):
-        response = self.client.post("/api/devices/", {"name": 1})
+        # 基础创建
+        response = self.client.post(
+            "/api/devices/",
+            {
+                "name": "1",
+                "desc": "test",
+                "custom_info": "test",
+                "sequence": 1,
+                "image_url": "test",
+            },
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data.get("name"), "1")
+        self.assertEqual(response.data.get("desc"), "test")
+        self.assertEqual(response.data.get("custom_info"), "test")
+        self.assertEqual(response.data.get("image_url"), "test")
+        self.assertEqual(response.data.get("sequence"), 1)
 
-        response = self.client.post("/api/devices/", {"name": 1, "category": 1000})
+        # 带有无效字段
+        response = self.client.post(
+            "/api/devices/",
+            {
+                "name": 1,
+                "status": True,
+                "is_super": True,
+                "last_connect_time": "2020-01-01 00:00:00",
+            },
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        device = Device.objects.filter(id=response.data.get("id")).first()
+        self.assertEqual(response.data.get("name"), "1")
+        self.assertEqual(device.status, False)
+        self.assertEqual(device.is_super, False)
+        self.assertEqual(device.last_connect_time, None)
+
+        # 创建设备分类不存在
+        response = self.client.post("/api/devices/", {"name": "1", "category": 1000})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data[0], "设备分类不存在！")
 
-        categroy = DeviceCategory.objects.create(name="2", create_user=self.user)
-        response = self.client.post("/api/devices/", {"name": 1, "category": categroy.id})
+        # 创建时指定设备分类
+        category = DeviceCategory.objects.create(name="2", create_user=self.user)
+        response = self.client.post("/api/devices/", {"name": "1", "category": category.id})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         # 嵌套创建数据流
-        body = {"name": 1, "category": categroy.id, "streams": [{"name": "s"}, {"name": "2"}]}
+        body = {"name": 1, "category": category.id, "streams": [{"name": "s"}, {"name": "2"}]}
         response = self.client.post("/api/devices/", body, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+        # 嵌套创建数据流+图表
+        body = {
+            "name": 1,
+            "category": category.id,
+            "streams": [{"name": "s"}, {"name": "2", "chart": {"name": "c"}}],
+        }
+        response = self.client.post("/api/devices/", body, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(response.data.get("streams", [])), 2)
+        self.assertEqual(response.data.get("streams")[1].get("chart").get("name"), "c")
+
+        # 嵌套创建数据流名称重复
         body["streams"] = [{"name": "s"}, {"name": "s"}]
         response = self.client.post("/api/devices/", body, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data[0], "同一设备数据流名称不能重复！")
 
     def test_device_detail(self):
-        body = {"name": 1, "streams": [{"name": "s"}, {"name": "2"}]}
+        body = {"name": 1, "streams": [{"name": "s"}, {"name": "2", "chart": {"name": "c"}}]}
         response = self.client.post("/api/devices/", body, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         device_id = response.data.get("id")
-
+        # stream 数量
         response = self.client.get(f"/api/devices/{device_id}/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data.get("streams", [])), 2)
 
         response = self.client.get(f"/api/devices/{device_id}/streams/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 2)
 
+        # chart 数量
+        response = self.client.get(f"/api/devices/{device_id}/charts/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # stream的show_chart=False
+        self.assertEqual(len(response.data), 0)
+
+        # perms
+        response = self.client.get(f"/api/devices/{device_id}/perms/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        perms = response.data
+        self.assertEqual(
+            set(response.data), {"view_device", "change_device", "delete_device", "control"}
+        )
+
+        # 无权限用户访问
         self.client.force_authenticate(user=self.user2)
         response = self.client.get(f"/api/devices/{device_id}/")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_device_perms(self):
-        response = self.client.post("/api/devices/", {"name": 1}, format="json")
+        response = self.client.post("/api/devices/", {"name": "1"}, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         device_id = response.data.get("id")
-        response = self.client.get(f"/api/devices/{device_id}/perms/")
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         self.client.force_authenticate(user=self.user2)
         response = self.client.get(f"/api/devices/{device_id}/")
@@ -146,25 +209,66 @@ class DeviceAPITestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_device_update(self):
-        response = self.client.post("/api/devices/", {"name": 1}, format="json")
+        response = self.client.post("/api/devices/", {"name": "1"}, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         device_id = response.data.get("id")
-        response = self.client.put(f"/api/devices/{device_id}/", {"name": 2})
+        category = DeviceCategory.objects.create(name="c0", create_user=self.user)
+        response = self.client.put(
+            f"/api/devices/{device_id}/",
+            {
+                "name": "2",
+                "desc": "test",
+                "custom_info": "test",
+                "sequence": 1,
+                "image_url": "test",
+                "status": True,
+                "is_super": True,
+                "category": category.id,
+                "last_connect_time": "2020-01-01 00:00:00",
+            },
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
         device = Device.objects.filter(id=device_id).first()
+        self.assertEqual(device.name, "2")
+        self.assertEqual(device.desc, "test")
+        self.assertEqual(device.custom_info, "test")
+        self.assertEqual(device.sequence, 1)
+        self.assertEqual(device.image_url, "test")
+        self.assertEqual(device.category, category)
 
+        # 无效字段
+        self.assertEqual(device.status, False)
+        self.assertEqual(device.is_super, False)
+        self.assertEqual(device.last_connect_time, None)
+
+        # 只有访问权限的用户无法修改
         self.client.force_authenticate(user=self.user2)
         assign_perm("view_device", self.user2, device)
-        response = self.client.put(f"/api/devices/{device_id}/", {"name": 3})
+        response = self.client.put(f"/api/devices/{device_id}/", {"name": "3"})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
+        # 赋予修改权限后可以修改
         assign_perm("change_device", self.user2, device)
-        response = self.client.put(f"/api/devices/{device_id}/", {"name": 3})
+        response = self.client.put(f"/api/devices/{device_id}/", {"name": "3"})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get("name"), "3")
+
+        # 不能修改共享设备的设备分类
+        category = DeviceCategory.objects.create(name="c1", create_user=self.user2)
+        response = self.client.put(f"/api/devices/{device_id}/", {"category": category.id})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data[0], "不可修改设备分类！")
+
+        # 不允许嵌套修改 streams
+        response = self.client.put(
+            f"/api/devices/{device_id}/",
+            {"streams": [{"name": "s"}, {"name": "2", "chart": {"name": "c"}}]},
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data.get("streams", [])), 0)
 
     def test_device_streams(self):
-        response = self.client.post("/api/devices/", {"name": 1}, format="json")
+        response = self.client.post("/api/devices/", {"name": "1"}, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         device_id = response.data.get("id")
         device = Device.objects.filter(id=device_id).first()
@@ -173,6 +277,24 @@ class DeviceAPITestCase(APITestCase):
         response = self.client.get(f"/api/devices/{device_id}/streams/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 2)
+
+    def test_device_charts(self):
+        response = self.client.post("/api/devices/", {"name": "1"}, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        device_id = response.data.get("id")
+        device = Device.objects.filter(id=device_id).first()
+        # 只有 stream.show_chart = True 才会返回
+        stream = Stream.objects.create(create_user=self.user, device=device, name="1", show_chart=True)
+        Chart.objects.create(create_user=self.user, stream=stream, name="1")
+        response = self.client.get(f"/api/devices/{device.id}/charts/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+        stream.show_chart = False
+        stream.save()
+        response = self.client.get(f"/api/devices/{device.id}/charts/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
 
     def test_device_delete(self):
         response = self.client.post("/api/devices/", {"name": 1}, format="json")
@@ -183,9 +305,15 @@ class DeviceAPITestCase(APITestCase):
 
         response = self.client.post("/api/devices/", {"name": 1}, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
         device_id = response.data.get("id")
         device = Device.objects.filter(id=device_id).first()
+        # 只有访问权限的用户无法删除
         assign_perm("view_device", self.user2, device)
         self.client.force_authenticate(user=self.user2)
         response = self.client.delete(f"/api/devices/{device_id}/")
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        # 赋予修改权限后可以删除
+        assign_perm("delete_device", self.user2, device)
+        response = self.client.delete(f"/api/devices/{device_id}/")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- link/apps/device/tests/test_stream.py
+++ link/apps/device/tests/test_stream.py
@@ -49,38 +49,78 @@ class StreamAPITestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         device_id = response.data.get("id")
 
-        response = self.client.post("/api/streams/", {"name": "1", "device": device_id})
+        # 普通创建
+        response = self.client.post(
+            "/api/streams/",
+            {
+                "name": "1",
+                "device": device_id,
+                "data_type": "number",
+                "unit": "test",
+                "unit_name": "test",
+                "show": True,
+                "icon": "test",
+                "color": "test",
+                "save_data": True,
+                "show_chart": True,
+            },
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data.get("name"), "1")
+        self.assertEqual(response.data.get("device"), device_id)
+        self.assertEqual(response.data.get("data_type"), "number")
+        self.assertEqual(response.data.get("unit"), "test")
+        self.assertEqual(response.data.get("unit_name"), "test")
+        self.assertEqual(response.data.get("icon"), "test")
+        self.assertEqual(response.data.get("color"), "test")
+        self.assertTrue(response.data.get("show"))
+        self.assertTrue(response.data.get("save_data"))
+        self.assertTrue(response.data.get("show_chart"))
 
+        # 数据流名称重复创建
         response = self.client.post("/api/streams/", {"name": "1", "device": device_id})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data["non_field_errors"][0], "同一设备数据流名称不能重复！")
 
+        # 设备为空创建
         response = self.client.post("/api/streams/", {"name": "2"})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data["device"][0], "该字段是必填项。")
 
+        # 没有修改设备权限创建
         self.client.force_authenticate(user=self.user2)
         response = self.client.post("/api/streams/", {"name": "12", "device": device_id})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # 有修改设备权限创建
         device = Device.objects.filter(id=device_id).first()
         assign_perm("change_device", self.user2, device)
         response = self.client.post("/api/streams/", {"name": "12", "device": device_id})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+        # 错误的数据类型创建
+        response = self.client.post(
+            "/api/streams/",
+            {
+                "name": "1",
+                "device": device_id,
+                "data_type": "test",
+                "show": "test",
+                "save_data": "test",
+                "show_chart": "test",
+            },
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["data_type"][0], "“test” 不是合法选项。")
+        self.assertEqual(response.data["show"][0], "Must be a valid boolean.")
+        self.assertEqual(response.data["save_data"][0], "Must be a valid boolean.")
+        self.assertEqual(response.data["show_chart"][0], "Must be a valid boolean.")
+
     def test_stream_create_with_chart(self):
+        # 嵌套创建图表
         response = self.client.post("/api/devices/", {"name": 1}, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         device_id = response.data.get("id")
-
-        response = self.client.post(
-            "/api/streams/", {"name": "1", "device": device_id, "show_chart": True}
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        stream_id = response.data.get("id")
-        self.assertTrue(response.data.get("save_data"))
-        chart = Chart.objects.filter(streams=stream_id).first()
-        self.assertIsNotNone(chart)
 
         response = self.client.post(
             "/api/streams/",
@@ -88,16 +128,17 @@ class StreamAPITestCase(APITestCase):
                 "name": "2",
                 "device": device_id,
                 "show_chart": True,
-                "chart_info": {"title": "test", "name": "test", "option": "test"},
+                "chart": {"title": "test", "name": "test", "option": "test"},
             },
-            format="json",
+            format = "json"
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         stream_id = response.data.get("id")
-        chart = Chart.objects.filter(streams=stream_id).first()
+        chart = Chart.objects.filter(stream=stream_id).first()
         self.assertIsNotNone(chart)
         self.assertEqual(chart.title, "test")
         self.assertEqual(chart.name, "test")
+        self.assertEqual(chart.option, "test")
 
     def test_stream_update(self):
         response = self.client.post("/api/devices/", {"name": 1}, format="json")
@@ -106,25 +147,62 @@ class StreamAPITestCase(APITestCase):
         stream1 = Stream.objects.create(name="s1", device=device, create_user=self.user)
         stream2 = Stream.objects.create(name="s2", device=device, create_user=self.user2)
 
+        # 正常修改
+        response = self.client.put(f"/api/streams/{stream1.id}/", {
+            "name": "test",
+            "unit": "test",
+            "unit_name": "test",
+            "show": True,
+            "icon": "test",
+            "color": "test",
+            "save_data": True,
+            "show_chart": True,
+        }, )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get("name"), "test")
+        self.assertEqual(response.data.get("unit"), "test")
+        self.assertEqual(response.data.get("unit_name"), "test")
+        self.assertEqual(response.data.get("icon"), "test")
+        self.assertEqual(response.data.get("color"), "test")
+        self.assertTrue(response.data.get("show"))
+        self.assertTrue(response.data.get("save_data"))
+        self.assertTrue(response.data.get("show_chart"))
+
+        # 错误类型
+        response = self.client.put(f"/api/streams/{stream1.id}/", {
+            "show": "test",
+            "save_data": "test",
+            "show_chart": "test",
+        }, )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["show"][0], "Must be a valid boolean.")
+        self.assertEqual(response.data["save_data"][0], "Must be a valid boolean.")
+        self.assertEqual(response.data["show_chart"][0], "Must be a valid boolean.")
+
+        # 重名修改
         response = self.client.put(f"/api/streams/{stream1.id}/", {"name": "s2"})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data["non_field_errors"][0], "同一设备数据流名称不能重复！")
 
-        response = self.client.put(f"/api/streams/{stream1.id}/", {"name": "s3"})
+        # 修改设备、数据类型、图表
+        other_device =  Device.objects.first()
+        response = self.client.put(
+            f"/api/streams/{stream1.id}/", {"device": other_device.id, "chart": {"title": "test"}}, format="json"
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get("device"), device.id)
+        self.assertEqual(response.data.get("chart").get("title"), "test")
 
-        response = self.client.put(f"/api/streams/{stream1.id}/", {"device": 1})
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        response = self.client.put(f"/api/streams/{stream1.id}/", {"data_type": "char"})
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
+        # 没有修改设备权限修改
         self.client.force_authenticate(user=self.user2)
         response = self.client.put(f"/api/streams/{stream2.id}/", {"name": "cs"})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        # 有修改设备权限修改
         assign_perm("view_device", self.user2, device)
         assign_perm("change_device", self.user2, device)
         response = self.client.put(f"/api/streams/{stream2.id}/", {"name": "cs"})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get("name"), "cs")
 
     def test_stream_update_with_chart(self):
         response = self.client.post("/api/devices/", {"name": 1}, format="json")
@@ -136,7 +214,7 @@ class StreamAPITestCase(APITestCase):
                 "name": "2",
                 "device": device_id,
                 "show_chart": True,
-                "chart_info": {"title": "test", "name": "test", "option": "test"},
+                "chart": {"title": "test", "name": "test", "option": "test"},
             },
             format="json",
         )
@@ -149,30 +227,13 @@ class StreamAPITestCase(APITestCase):
                 "name": "3",
                 "device": device_id,
                 "show_chart": True,
-                "chart_info": {"title": "test1", "name": "test1", "option": "test"},
+                "chart": {"title": "test1", "name": "test1", "option": "test1"},
             },
             format="json",
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        chart = Chart.objects.filter(streams=stream_id).first()
+        chart = Chart.objects.filter(stream=stream_id).first()
         self.assertIsNotNone(chart)
         self.assertEqual(chart.title, "test1")
         self.assertEqual(chart.name, "test1")
-
-        response = self.client.put(
-            f"/api/streams/{stream_id}/",
-            {
-                "name": "3",
-                "device": device_id,
-                "save_data": False,
-                "show_chart": False,
-                "chart_info": {"title": "test2", "name": "test2", "option": "test"},
-            },
-            format="json",
-        )
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data.get("save_data"), False)
-        chart = Chart.objects.filter(streams=stream_id).first()
-        self.assertIsNotNone(chart)
-        self.assertNotEqual(chart.title, "test2")
-        self.assertNotEqual(chart.name, "test2")
+        self.assertEqual(chart.option, "test1")
